7ad62cb8f44df34000fc872949c0a3951ac3acc6
tjyuki <tamiyukiko12@gmail.com>
2016-05-30 12:04:35 -0600

    set up admin controller

--- public/admin/controllers/adminCtrl.js
+++ public/admin/controllers/adminCtrl.js
@@ -1,4 +1,33 @@
-angular.module('janet').controller('adminCtrl', function($scope){
+angular.module('janet').controller('adminCtrl', function($scope, adminService){
 
+  $scope.showProducts = function(){
+    adminService.getProducts().then(function(response){
+      console.log(response);
+      $scope.products = response;
+    });
+  };
+
+  $scope.showProducts();
+
+$scope.addNewProduct = function(newProduct){
+  adminService.createProduct(newProduct).then(function(response){
+    $scope.showProducts();
+    $scope.newProduct = response;
+  });
+};
+
+$scope.editProduct = function(product){
+  adminService.updateSingleProduct(product).then(function(){
+    $scope.showProducts();
+  });
+};
+
+$scope.id = product._id;
+
+$scope.deleteProduct = function(id){
+  adminService.deleteProduct(id).then(function(){
+    $scope.showProducts();
+  });
+};
 
 });
