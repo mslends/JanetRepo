ac2ab3780ede82d329a487fe97639744dae44fac
Jace Briggs <bri08006@byui.edu>
2016-06-01 16:13:25 -0600

    created footer and changed nav control over to navCtrl

--- public/customers/directives/navDirective.js
+++ public/customers/directives/navDirective.js
@@ -1,8 +1,9 @@
 angular.module('janet').directive('navDirective', function(){
   return {
-    controller:'homeCtrl',
     restrict: 'E',
-    templateUrl: './customers/views/navView.html'
+    templateUrl: './customers/views/navView.html',
+    controller:'navCtrl'
+
   }
 
 })
