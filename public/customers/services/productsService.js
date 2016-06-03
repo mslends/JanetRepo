47ffc3be1261ab30eb4ac10415f67ec48684244c
Jace Briggs <bri08006@byui.edu>
2016-06-03 16:45:11 -0600

    for real though.  nav bar and the whole no pushy thing

--- public/customers/services/productsService.js
+++ public/customers/services/productsService.js
@@ -1,4 +1,12 @@
 angular.module('janet').service('productsService', function($http){
 
+this.getProducts = function(){
+  return $http({
+    method: "GET",
+    url: "/api/products"
+  }).then(function(response){
+    return response.data;
+  });
+};
 
 });
