c998c232baf2f959081b37905e62a66d78283129
A-Reum Jung <areumjung1987@gmail.com>
2016-06-06 01:29:35 -0600

    subtotal, shipping and total price calculation done

--- public/customers/controllers/cartCtrl.js
+++ public/customers/controllers/cartCtrl.js
@@ -1,20 +1,55 @@
 angular.module('janet').controller('cartCtrl', function($scope, cartService, orderService){
+  
 
   $scope.modalShown = false;
   $scope.toggleModal = ()=>{
     $scope.modalShown = !$scope.modalShown;
   };
 
-$scope.cart = cartService.cart
+  $scope.cart = cartService.cart
+  console.log($scope.cart)
 
-$scope.getOneUser = ()=>{
-  customerService.getOneUser($scope.user._id).then((response)=>{
-    console.log(response);
-    $scope.user = response;
-  });
-};
+  $scope.getOneUser = ()=>{
+    customerService.getOneUser($scope.user._id).then((response)=>{
+      $scope.user = response;
+    });
+  };
 
 
+
+  $scope.removeItem = (item)=>{
+    cartService.removeItem(item);
+  };
+
+  $scope.addQty = (item)=>{
+    cartService.addQty(item);
+  };
+
+  $scope.removeQty = (item)=>{
+    cartService.removeQty(item);
+  };
+
+
+  $scope.shipping = 0;
+  $scope.subtotal = 0;
+  $scope.total = 0;
+  $scope.totalCost = ()=>{
+    $scope.shipping = 0;
+    $scope.subtotal = 0;
+    $scope.total = 0;
+    for(var i = 0; i < $scope.cart.length; i++){
+      $scope.subtotal += ($scope.cart[i].product.discountPrice * $scope.cart[i].productQty);
+      $scope.shipping  += ($scope.subtotal * 0.02) + 5;
+      $scope.total = $scope.subtotal + $scope.shipping;
+    }
+  };
+  $scope.totalCost();
+
+  $scope.createOrder =()=>{
+    orderService.createOrder().then((response)=>{
+      $scope.newOrder = response;
+    })
+  }
 
 
 })
--- public/customers/services/cartService.js
+++ public/customers/services/cartService.js
@@ -3,10 +3,46 @@ angular.module('janet').service('cartService', function($http, $stateParams){
 
   this.cart = [];
 
+
   this.addToCart = (product, productQty)=>{
     this.cart.push({product, productQty});
     return this.cart;
   };
+
+
+  this.removeItem = (product)=>{
+    for(var i = 0; i < this.cart.length; i++){
+      if(this.cart[i] === product){
+        this.cart.splice(this.cart[i], 1);
+        return this.cart;
+      }
+    }
+  };
+
+  this.addQty = (product)=>{
+    for(var i = 0; i < this.cart.lenght; i++){
+      if(this.cart[i] === product){
+      this.cart[i].productQty++;
+      return this.cart;
+      }
+    }
+  };
+
+  this.removeQty = (product)=>{
+    for(var i = 0; i < this.cart.lenght; i++){
+      if(this.cart[i] === product){
+        if(this.cart[i].productQty !== 1){
+          this.cart[i]--;
+          return this.cart;
+        }
+      }
+    }
+  };
+
+
+
+
+
 
 
 });
