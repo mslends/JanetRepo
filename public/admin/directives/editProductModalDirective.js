8c8e5cc55be9f972100ea1d18b2899b87aef186a
Jace Briggs <bri08006@byui.edu>
2016-06-09 09:40:01 -0600

    merging other changes

--- public/admin/directives/editProductModalDirective.js
+++ public/admin/directives/editProductModalDirective.js
@@ -6,7 +6,7 @@ angular.module('janet')
       // scope: {
       //   productData: "="
       // },
-      controller: "editProductModalCtrl"
-    }
+    //   controller: "editProductModalCtrl"
+};
 
   });
--- public/customers/controllers/settingsCtrl.js
+++ public/customers/controllers/settingsCtrl.js
@@ -1,23 +1,25 @@
 angular.module('janet').controller('settingsCtrl', function($scope, customerService, user, $state) {
 
+
   $scope.user = user;
 
   $scope.updateUser = (user)=>{
     customerService.updateUser(user._id, user).then((response)=>{
     $scope.updatedUser = response;
     alert('Your information has been updated!')
+    $scope.getOneUser();
     });
   };
 
   $scope.getOneUser = ()=>{
     if($scope.user){
       customerService.getOneUser($scope.user._id).then((response)=>{
-          console.log(response);
-        $scope.oneUser = response;
-        $scope.orders = $scope.oneUser.orderHistory;
+        $scope.user = response;
+        $scope.orders = $scope.user.orderHistory;
       });
     }
-  }();
+  };
+  $scope.getOneUser();
 
 
 
