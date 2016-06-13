09783442d18272a43a3abe88730cb1de4da31ee9
tjyuki <tamiyukiko12@gmail.com>
2016-06-13 17:09:33 -0600

    need to pull from master

--- public/admin/controllers/adminCtrl.js
+++ public/admin/controllers/adminCtrl.js
@@ -1,33 +1,34 @@
-angular.module('janet').controller('adminCtrl', function($scope, $stateParams, adminService){
+angular.module('janet').controller('adminCtrl', function($scope, $stateParams, adminService) {
 
-  console.log("adminCtrl working");
+    console.log("adminCtrl working");
 
-  $scope.showProducts = () => {
-    adminService.getProducts().then(function(response){
-      console.log(response);
-      $scope.products = response;
-    });
-  };
+    $scope.showProducts = () => {
+        adminService.getProducts().then(function(response) {
+            console.log(response);
+            $scope.products = response;
+        });
+    };
 
-  $scope.showProducts();
-
-$scope.addNewProduct = function(newProduct){
-  adminService.createProduct(newProduct).then(function(response){
     $scope.showProducts();
-    $scope.newProduct = response;
-    $scope.newProduct = {};
-    $scope.newProductModalShown = !$scope.newProductModalShown;
-    alert(newProduct.name + " has been added to inventory!");
-  });
-};
 
-$scope.deleteProduct = function(product){
-  $scope.confirmDelete = confirm(product.name + " will be removed from inventory. Click OK to confirm.");
-  if($scope.confirmDelete === true){
-  adminService.deleteSingleProduct(product).then(function(){
-    $scope.showProducts();
-  });
-};
+    $scope.addNewProduct = function(newProduct) {
+        adminService.createProduct(newProduct).then(function(response) {
+            $scope.showProducts();
+            $scope.newProduct = response;
+            $scope.newProduct = {};
+            $scope.newProductModalShown = !$scope.newProductModalShown;
+            alert(newProduct.name + " has been added to inventory!");
+        });
+    };
+
+    $scope.deleteProduct = function(product) {
+        $scope.confirmDelete = confirm(product.name + " will be removed from inventory. Click OK to confirm.");
+        if ($scope.confirmDelete === true) {
+            adminService.deleteSingleProduct(product).then(function() {
+                $scope.showProducts();
+            });
+        }
+    };
 
 
 
